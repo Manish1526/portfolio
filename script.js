813df50049c477fde1706f66c66ff93ff5bdad87
Manish Kumar <152203532+Manish1526@users.noreply.github.com>
2024-01-18 22:24:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* ================ Typing Animation ===================== */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Batching Plant Operator",
+    "Plant Supervisor",
+    "Plant Forman",
+    "Maintenance Co-ordinator",
+    "Team Leader",
+    "Concrete Dispatcher",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
